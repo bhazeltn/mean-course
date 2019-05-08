a3a9c0c6c4d558e245ee05ce38e4ed5934b67390
Bradley Hazelton <bradley@thehazeltons.ca>
2019-05-08 12:25:00 -0600

    Material design on post-create component

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -3,7 +3,9 @@ import { post } from 'selenium-webdriver/http';
 
 @Component({
   selector: 'app-post-create',
-  templateUrl: './post-create.component.html'})
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css'],
+})
 export class PostCreateComponent {
   newPost = 'No Content';
   enteredValue = '';
