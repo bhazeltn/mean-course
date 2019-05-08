92d3ae623525681be4035efb5ba96363598ff5c2
Bradley Hazelton <bradley@thehazeltons.ca>
2019-05-08 14:29:38 -0600

    Create post, save to array, add to page

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,5 +1,4 @@
-import { Component } from '@angular/core';
-import { post } from 'selenium-webdriver/http';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-post-create',
@@ -7,10 +6,14 @@ import { post } from 'selenium-webdriver/http';
   styleUrls: ['./post-create.component.css'],
 })
 export class PostCreateComponent {
-  newPost = 'No Content';
-  enteredValue = '';
+
+  enteredContent = '';
+  enteredTitle = '';
+  @Output() postCreated = new EventEmitter();
 
   onAddPost() {
-    this.newPost = this.enteredValue;
+    const post = {title: this.enteredTitle,
+      content: this.enteredContent};
+      this.postCreated.emit(post);
   }
 }
